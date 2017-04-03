de60e24b16f9b1e1a6573c888fc56b09837da413
Lisa Durand <lisa@superdudu.net>
2017-04-03 16:08:11 +0200

    Launch Mux server with TDD

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "net/http"
+
 func Sum(a int, b int) int {
   return a + b
+}
+
+func main() {
+  http.ListenAndServe(":8080", nil)
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,23 @@
 package main
 
-import "testing"
+import (
+  "testing"
+  "net"
+)
 
 func TestSum(t *testing.T) {
   total := Sum(5, 5)
   if total != 10 {
     t.Errorf("Sum was incorrect, got %d, want %d", total, 10)
+  }
+}
+
+func TestLaunchServer(t *testing.T) {
+  go main()
+
+  _, err := net.Dial("tcp", "localhost:8080")
+
+  if err != nil {
+    t.Errorf("Couldn't launch server: %s", err)
   }
 }
