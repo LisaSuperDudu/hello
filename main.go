5ebeaa5461bfb66868aa5d388ed17e7c8be728d7
Lisa Durand <lisa@superdudu.net>
2017-04-05 17:57:00 +0200

    Launch TCP server that doesnt respond to client request (test fail)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "net/http"
   "fmt"
+  "net"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +11,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  // http.HandleFunc("/", handler)
+  // http.ListenAndServe(":8080", nil)
+  listener, err := net.Listen("tcp", "localhost:8080")
+
+  if err != nil {
+    fmt.Printf("Couldn't listen for incoming connection : %s", err)
+  }
+
+  fmt.Println("Listening for incoming connection")
+
+  for {
+    conn, err := listener.Accept()
+    if err != nil {
+      fmt.Printf("Couldn't accept incoming connection: %s", err)
+    }
+    conn.Close()
+  }
+
+
 }
--- main_test.go
+++ main_test.go
@@ -5,17 +5,18 @@ import (
   "net"
   "fmt"
   "bufio"
-  "strings"
 )
 
 func assertEquals(expected string, actual string, t *testing.T) {
-  if strings.TrimSpace(actual) != strings.TrimSpace(expected) {
+  if actual != expected {
     t.Errorf("We expected: %s but we got: %s", expected, actual)
   }
 }
 
 func TestServer(t *testing.T) {
   go main()
+  fmt.Printf("coucou 4")
+
 
   //Test Launch Server
   conn, err := net.Dial("tcp", "localhost:8080")
@@ -25,17 +26,37 @@ func TestServer(t *testing.T) {
     return
   }
 
+  //Test that server response to a Get request
+  fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+
+  fmt.Printf("coucou 7")
   response := bufio.NewReader(conn)
 
-  fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+  fmt.Printf("coucou 8")
+
+  // if len(response.buf) == 0 {
+  //   t.Errorf("Couldn't read response")
+  //   return
+  // }
+
   statusLine, err := response.ReadString('\n')
-  assertEquals("HTTP/1.0 200 OK", statusLine, t)
 
-  header, err := response.ReadString('\n')
-
-  for header != "\r\n" {
-    header, err = response.ReadString('\n')
+  if statusLine == "" {
+    t.Errorf("Couldn't read statusLine")
+    return
   }
+
+  fmt.Printf("coucou 9")
+  assertEquals("HTTP/1.0 kjkhjk00 OK\n", statusLine, t)
+
+  fmt.Printf("coucou 10")
+  currentLine, err := response.ReadString('\n')
+
+  headerBodySeparator := "\r\n"
+  for currentLine != headerBodySeparator {
+    currentLine, err = response.ReadString('\n')
+  }
+  fmt.Printf("coucou 11")
 
   body, err := response.ReadString('\n')
   assertEquals("how did we get here?", body, t)
